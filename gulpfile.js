e2bd45c81ec81a700cb01a9505639499f8832582
yat8823jp <yat@yat-net.com>
2015-01-15 10:26:17 +0900

    browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -10,37 +10,48 @@ var browserSync = require('browser-sync');
 * 変更があり次第imagesフォルダー以下の画像の圧縮を実行するタスク
 * */
 gulp.task("watchTask", function() { // 「watchTask」という名前のタスクを登録
-    gulp.watch("images/**", function() {   // imagesフォルダ以下のファイルを監視
-        gulp.src("images/*.png")
-            .pipe(imagemin())
-            .pipe(gulp.dest("minified_image"));
-    });
+	gulp.watch("images/**", function() {   // imagesフォルダ以下のファイルを監視
+		gulp.src("images/*.png")
+		.pipe(imagemin())
+		.pipe(gulp.dest("minified_image"));
+	});
 });
 
-//Webサーバー
+// Webサーバー
 gulp.task('webserver', function() {
-  gulp.src('./') //Webサーバーで表示するサイトのルートディレクトリを指定
-    .pipe(webserver({
-      livereload: true, //ライブリロードを有効に
-      directoryListing:true //ディレクトリ一覧を表示するか。オプションもあり
-/*
-      proxies: [
-        {
-          source: './',
-          target: 'http://localhost:8888/sample/'
-        }
-      ]
-*/
+	gulp.src('./gulp') //Webサーバーで表示するサイトのルートディレクトリを指定
+	.pipe(webserver({
+		livereload: true, //ライブリロードを有効に
+		directoryListing:true, //ディレクトリ一覧を表示するか。オプションもあり
+		proxies: [{
+			source: '/gulp',
+			target: 'http://localhost:8888/gulp'
+		}]
     }));
 });
 
 gulp.task('browser-sync', function() {
-    browserSync({
-        server: {
-            baseDir: "./",
-            proxy: 'localhost:8888'
-        }
+	gulp.src('./gulp/') //Webサーバーで表示するサイトのルートディレクトリを指定
+	
+	browserSync(null,{
+		// server: {
+		// 	baseDir: "./"
+		// }
+		proxy: "localhost:8888"
     });
+
 });
 
-gulp.task('default', ['webserver']);
+gulp.task('bsReload', function() {
+	// gulp.src('./');
+	// baseDir: "./";
+	// return gulp.src('./');
+	browserSync.reload();
+})
+
+gulp.task('default', ['bsReload','browser-sync'],function(){
+	// gulp.watch('./**/*.php',['bsReload',browserSync.reload]);
+	gulp.watch('./**/*.html',['bsReload']);
+	gulp.watch('./**/*.js',['bsReload']);
+	gulp.watch('./**/*.php',['bsReload']);
+});
