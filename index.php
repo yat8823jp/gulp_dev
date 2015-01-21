a83af5d8d4d9a69609c822c27458e12253a61190
久保 知己 <kubo_tomoki_xa_gn@cyberagent.co.jp>
2015-01-21 15:22:46 +0900

    add dummy file

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+</head>
+<body>
+  <h1>Title</h1>
+  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis placeat nesciunt minima, repellendus odio, accusamus cum aliquid aspernatur vitae similique error amet quibusdam eum. Aliquam aut libero odit, vero et.</p>
+</body>
+</html>
